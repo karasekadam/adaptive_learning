f4ce7e49bd65fd24860788e1b2664d820a387cc1
unknown <karasekad@gmail.com>
2024-04-01 18:31:38 +0200

    add parameters

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 import json
-from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score
+from sklearn.linear_model import LinearRegression
+import seaborn as sb
 # from simpletransformers.classification import ClassificationModel, ClassificationArgs
 
 written_answers = pd.read_csv('mat-writtenAnswer.csv', sep=";")
@@ -12,6 +14,7 @@ word_levels = pd.read_csv('word_levels.csv', sep=";")
 
 written_answers_merged = pd.merge(written_answers, system_resource_set, left_on="rs", right_on="id")
 all_data = pd.merge(written_answers_merged, system_kc, left_on="parent", right_on="id", suffixes=("_resource", "_kc"))
+
 
 def parse_answer(x):
     data = json.loads(x)
@@ -81,11 +84,90 @@ def question_type():
     plt.show()
 
 
+zlomky_kc = [38, 55, 44, 43, 45, 172, 118, 111, 168, 41, 42, 329]
+zlomky_data = all_data[all_data["id_kc"].isin(zlomky_kc)]
 
-# question_len()
+
+def filter_out_img(df: pd.DataFrame) -> pd.DataFrame:
+    rows_to_drop = []
+    for i, row in df.iterrows():
+        question = json.loads(row["question"])
+        for question_part in question:
+            if "img" == question_part[0]:
+                rows_to_drop.append(i)
+    df.drop(rows_to_drop, inplace=True)
+    return df
 
 
-question_type()
+zlomky_data = filter_out_img(zlomky_data)
+# question_type()
+
+
+def parameters(df_data: pd.DataFrame):
+    df_data["plus"] = df_data["question_text"].map(lambda x: x.count("+"))
+    df_data["minus"] = df_data["question_text"].map(lambda x: x.count("-"))
+    df_data["times"] = df_data["question_text"].map(lambda x: x.count("cdot"))
+    df_data["div"] = df_data["question_text"].map(lambda x: x.count(":"))
+    df_data["frac"] = df_data["question_text"].map(lambda x: x.count("frac"))
+    df_data["question_len"] = df_data["question_text"].map(lambda x: len(x))
+    df_data["answer_len"] = df_data["answer_text"].map(lambda x: len(x))
+    df_data["answer_float"] = df_data["answer_text"].map(lambda x: not "/" in x)
+
+
+def merge_question(df_data: pd.DataFrame):
+    question = ""
+    for question_part in df_data["question"]:
+        question += question_part[1]
+
+
+def process_question_json(row):
+    question_stream = row["question"]
+    question = json.loads(question_stream)
+    question_text = ""
+    for question_part in question:
+        if question_part[0] == "latex":
+            question_text += " " + question_part[1]
+    return question_text
+
+
+def process_answer_json(row):
+    answer_stream = row["question"]
+    answer = json.loads(answer_stream)
+    answer_text = ""
+    for answer_part in answer:
+        if answer_part[0] == "input":
+            answer_text += " " + answer_part[1]["answer"][0]
+    return answer_text
+
+
+zlomky_data["question_text"] = zlomky_data.apply(process_question_json, axis=1)
+zlomky_data["answer_text"] = zlomky_data.apply(process_answer_json, axis=1)
+
+columns_to_keep = ["resourceId", "rs", "question", "question_text", "answer_text", "explanation_x", "errorRate", "responseTime"]
+zlomky_data = zlomky_data[columns_to_keep]
+
+parameters(zlomky_data)
+
+
+def linear_regression(zlomky_data: pd.DataFrame):
+    model = LinearRegression()
+    X = zlomky_data[["plus", "minus", "times", "div", "frac", "question_len", "answer_len", "answer_float"]]
+    y = zlomky_data["errorRate"]
+    model.fit(X, y)
+    predictions = model.predict(X)
+
+    print(model.score(X, y))
+    coefficient_of_dermination = r2_score(y, model.predict(X))
+    print(coefficient_of_dermination)
+    print(X.corr())
+    print(model.coef_)
+
+    dataplot = sb.heatmap(X.corr(), cmap="YlGnBu", annot=True)
+    sb.heatmap(X.corr(), cmap="Blues", annot=True)
+
+
+linear_regression(zlomky_data)
+
 """# Enabling regression
 # Setting optional model configuration
 model_args = ClassificationArgs()
